afd5fca562ffbb0185b9f293d6176af2a1c44ef2
Md. Aminul Islam Shazid <70378126+ami-null@users.noreply.github.com>
2026-09-14 21:34:37 +0600

    create plots

--- 01_intro/figures/propellant_scatterplot.r
+++ 01_intro/figures/propellant_scatterplot.r
@@ -6,12 +6,46 @@ propellant <- data.frame(
                               2575.00,2357.90,2256.70,2165.20,2399.55,1779.80,2336.75,
                               1765.30,2053.50,2414.40,2200.50,2654.20,1753.70)
     )
+
+fit <- lm(shear ~ age, data = propellant)
+propellant$fitted <- fitted(fit)
+
+
 ggplot(propellant, aes(x = age, y = shear)) +
-      geom_point(size = 2, color = "#1b9e77") +
+      geom_point(size = 1, color = "#1b9e77") +
       labs(x = "Age of Propellant (weeks)", y = "Shear Strength (psi)") +
       theme_minimal(base_size = 12) +
       theme(
             panel.background = element_rect(fill = "transparent", colour = NA),
             plot.background  = element_rect(fill = "transparent", colour = NA)
           )
-ggsave("propellant_scatter.pdf", bg = "transparent", width = 4.5, height = 3.2)
+ggsave("propellant_scatter.pdf", bg = "transparent", width = 4.5, height = 3.5)
+
+
+
+
+ggplot(propellant, aes(x = age, y = shear)) +
+    geom_point(size = 1, color = "#1b9e77") +
+    geom_smooth(method = "lm", se = FALSE, color = "#d95f02", linewidth = 0.8) +
+    labs(x = "Age of Propellant (weeks)", y = "Shear Strength (psi)") +
+    theme_minimal(base_size = 12) +
+    theme(
+        panel.background = element_rect(fill = "transparent", colour = NA),
+        plot.background  = element_rect(fill = "transparent", colour = NA)
+    )
+ggsave("propellant_fitted_line.pdf", bg = "transparent", width = 4.5, height = 3.5)
+
+
+
+
+ggplot(propellant, aes(x = age, y = shear)) +
+    geom_segment(aes(xend = age, yend = fitted), linetype = "dotted", color = "#e7298a", linewidth = 0.5) +
+    geom_smooth(method = "lm", se = FALSE, color = "#d95f02", linewidth = 0.8) +
+    geom_point(size = 1, color = "#1b9e77") +
+    labs(x = "Age of Propellant (weeks)", y = "Shear Strength (psi)") +
+    theme_minimal(base_size = 12) +
+    theme(
+        panel.background = element_rect(fill = "transparent", colour = NA),
+        plot.background  = element_rect(fill = "transparent", colour = NA)
+    )
+ggsave("propellant_residuals.pdf", bg = "transparent", width = 4.5, height = 3.5)
